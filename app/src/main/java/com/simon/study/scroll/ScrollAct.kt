8528c2514937f72ef25ef05bda147bb0bb91284d
simondai <simon_dai@zhan.com>
2018-06-25 10:02:13 +0800

    scroll

--- app/src/main/java/com/simon/study/scroll/ScrollAct.kt
+++ app/src/main/java/com/simon/study/scroll/ScrollAct.kt
@@ -13,7 +13,9 @@ import kotlinx.android.synthetic.main.act_scroll.*
  *   version code 1.0
  *
  *
- *   scrollBy/scrollTo,移动的是view里面的内容（子view或者显示的内容）
+ *   scrollBy/scrollTo,移动的是父布局，所以负数的时候，子view向右移动（实际上是父布局向左移动）
+ *   scrollTo 方法和 scrollBy 方法移动的是 view 里面的内容（子控件或者是显示的内容），
+ *   并且移动的方向和方法的参数正负是相反的（也可以借助参考物来理解（父容器移动，子控件不移动，相对父容器来说，子控件移动的方向是与其相反））。
  *
  *
  */
@@ -24,13 +26,13 @@ class ScrollAct : AppCompatActivity() {
         setContentView(R.layout.act_scroll)
 
 
-        btnScrollTo.setOnClickListener {
-            (parentLayout as ViewGroup).scrollTo(-30, -30)
-        }
-
-        btnScrollBy.setOnClickListener {
-            (parentLayout as ViewGroup).scrollBy(-30, -30)
-        }
+//        btnScrollTo.setOnClickListener {
+//            (parentLayout as ViewGroup).scrollTo(-30, -30)
+//        }
+//
+//        btnScrollBy.setOnClickListener {
+//            (parentLayout as ViewGroup).scrollBy(-30, -30)
+//        }
 
     }
 }
---
+++ app/src/main/java/com/simon/study/scroll/ScrollLinearLayout.kt
@@ -0,0 +1,101 @@
+package com.simon.study.scroll
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.LinearLayout
+import android.widget.Scroller
+import android.widget.Switch
+import java.util.jar.Attributes
+
+/**
+ *   created by simon
+ *   date 2018/6/22
+ *   description:
+ *   version code 1.0
+ */
+class ScrollLinearLayout : LinearLayout {
+
+    private var scroller: Scroller? = null
+    private var lastX: Int = 0
+    private var lastY: Int = 0
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
+    constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : super(context, attributeSet, defStyleAttr) {
+        scroller = Scroller(context)
+    }
+
+
+    /**
+     * 重写父类的 computeScroll方法来判断滑动是否完成，进行对应处理
+     */
+    override fun computeScroll() {
+        super.computeScroll()
+        //如果滑动完成，返回false，否则返回true
+        if (scroller!!.computeScrollOffset()) {
+            scrollTo(scroller!!.currX, scroller!!.currY)
+            invalidate()
+        }
+    }
+
+
+    /**
+     * 重写touchEvent
+     *
+     * 1、初始化 Scroller 类的对象：Scroller scroller = new Scroller(context)
+     * 2、重写要滑动的 View 的 computeScroll() 方法
+     * 3、调用 startScroll(int startX, int startY, int dx, int dy)方法开始 View 的滑动，
+     * 参数分别为开始的位置和横纵方向滑动的位移，这个方法还有一个重载版本，多了一个参数用于控制滑动时间
+     *
+     */
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        var x: Int = event.x.toInt()
+        var y: Int = event.y.toInt()
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                scroller!!.startScroll(lastX, lastY, lastX - x, lastY - y)
+                invalidate()
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                scrollTo(-x, -y)
+                invalidate()
+            }
+
+            MotionEvent.ACTION_UP -> {
+                scroller!!.startScroll(scrollX, scrollY, -scrollX, -scrollY)
+                invalidate()
+            }
+        }
+
+        return true
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        return true
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
